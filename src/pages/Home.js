bbaea4e113d21212ae1408629ab4bde35ffe20f0
kenan-erol <kenan.erol@yale.edu>
2024-06-16 19:34:52 -0700

    functionally works

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,14 @@ class Home extends Component {
       todos: [],
     };
   }
+	deleteTodo = (id) => {
+		const todos = this.state.todos.filter((todo) => {
+		return todo.id !== id;
+		});
+		this.setState({
+		todos: todos,
+		});
+	};
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -21,11 +29,13 @@ class Home extends Component {
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
     // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
-    this.setState({
-      todos: new_list,
-    });
+	if (!this.state.todos.find(t => t.content === todo.content)) {
+		let new_list = [...this.state.todos, todo];
+		// Update the local state with the new array.
+		this.setState({
+		todos: new_list,
+		});
+	} return;
   };
   render() {
     return (
@@ -36,7 +46,7 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
